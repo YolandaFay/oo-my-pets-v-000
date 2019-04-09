afdb92d2d5736a1ea04a9bcb5753e4d7cc09d89b
Yolanda Fay <yolandafay@gmail.com>
2019-04-09 03:14:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,7 +63,7 @@ class Owner
   
   
   def pets    #is initialized with a pets attribute as a hash with 3 keys
-    @pets = {:fish => [], :dogs => [], :cats => []}
+    @pets = {:fishes => [], :dogs => [], :cats => []}
   end
   
   
